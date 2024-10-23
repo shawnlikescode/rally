b35a240b76d6b34f1c311761ef96c1bdffc8c5a0
shawnlikescode <shawnlockhart7@gmail.com>
2024-10-23 11:06:55 -0500

    setup database vercel postgres

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## TODO
 
-- [ ] Make it deploy
+- [X] Make it deploy
 - [ ] Add Auth (Clerk)
-- [ ] Setup a database
+- [X] Setup a database
 - [ ] Scaffold basic ui for creating a wake-up call 
 - [ ] Twilio integration
 - [ ] Cron for sending wake-up calls
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const data = await db.query.posts.findMany();
+
+  console.log(data);
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <h1>Wake-Up Call Project</h1>
